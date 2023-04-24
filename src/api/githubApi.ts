e425b68fd32116de5040191503773781f95b54ad
Hugo <hugo@boahstudio.com>
2023-04-24 00:36:36 -0300

    issues

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,5 +3,7 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
-    headers: {}
+    headers: {
+        Authorization: 'Bearer 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'
+    }
 })
--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,22 +1,42 @@
 import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
+import { Issue, State } from '../interfaces';
+import { FC } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+interface Props {
+    issue: Issue;
+}
+
+export const IssueItem: FC<Props> = ({ issue }) => {
+
+    const today = new Date();
+    const providedDate = new Date( issue.created_at );
+    const timeDiff = today.getTime() - providedDate.getTime();
+    const daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+
+    const navigate = useNavigate();
 
 
-export const IssueItem = () => {
     return (
-        <div className="card mb-2 issue">
+        <div className="card mb-2 issue"
+            onClick={ () => navigate(`/issues/issue/${ issue.number }`) }
+        >
             <div className="card-body d-flex align-items-center">
                 
-                <FiInfo size={30} color="red" />
-                {/* <FiCheckCircle size={30} color="green" /> */}
-
+                {
+                    issue.state === State.Open
+                        ? ( <FiInfo size={30} color="red" /> )
+                        : ( <FiCheckCircle size={30} color="green" /> )
+                }
+                
                 <div className="d-flex flex-column flex-fill px-2">
-                    <span>Suggestion: why not make accessing and changing the state possible globally?</span>
-                    <span className="issue-subinfo">#25581 opened 2 days ago by <span className='fw-bold'>segfaulty1</span></span>
+                    <span>{ issue.title }</span>
+                    <span className="issue-subinfo">{`#${ issue.number } opened ${ daysDiff } days ago by`} <span className='fw-bold'>{ issue.user.login }</span></span>
                 </div>
 
                 <div className='d-flex align-items-center'>
-                    <img src="https://avatars.githubusercontent.com/u/1933404?v=4" alt="User Avatar" className="avatar" />
-                    <span className='px-2'>2</span>
+                    <img src={ issue.user.avatar_url } alt="User Avatar" className="avatar" />
+                    <span className='px-2'>{ issue.comments }</span>
                     <FiMessageSquare />
                 </div>
 
