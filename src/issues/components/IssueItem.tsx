b822dbaa182550c4c994d5355504a8ed64513304
Hugo <hugo@boahstudio.com>
2023-04-25 19:06:10 -0300

    optimizations

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,7 +1,10 @@
-import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
-import { Issue, State } from '../interfaces';
 import { FC } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useQueryClient } from '@tanstack/react-query';
+import { FiInfo, FiMessageSquare, FiCheckCircle } from 'react-icons/fi';
+
+import { Issue, State } from '../interfaces';
+import { getIssueComments, getIssueInfo } from '../hooks/useIssue';
 
 interface Props {
     issue: Issue;
@@ -15,11 +18,37 @@ export const IssueItem: FC<Props> = ({ issue }) => {
     const daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
 
     const navigate = useNavigate();
+    const queryClient = useQueryClient();
 
+    const prefetchData = () => {
+
+        queryClient.prefetchQuery(
+            ['issue', issue.number ],
+            () => getIssueInfo( issue.number ),
+        );
+
+        queryClient.prefetchQuery(
+            ['issue', issue.number, 'comments' ],
+            () => getIssueComments( issue.number ),
+        );
+
+    }
+
+    const preSetData = () => {
+        queryClient.setQueryData(
+            ['issue', issue.number ],
+            issue,
+            {
+                updatedAt: new Date().getTime() + 100000
+            }
+        );
+    }
 
     return (
         <div className="card mb-2 issue"
             onClick={ () => navigate(`/issues/issue/${ issue.number }`) }
+            //onMouseEnter={ prefetchData }
+            onMouseEnter={ preSetData }
         >
             <div className="card-body d-flex align-items-center">
                 
