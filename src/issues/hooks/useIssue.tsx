6cc8f6b2f47e8310f5f28d49cc6cfb3f4ddba72d
Hugo <hugo@boahstudio.com>
2023-04-25 00:38:19 -0300

    Issues Comments

--- src/issues/hooks/useIssue.tsx
+++ src/issues/hooks/useIssue.tsx
@@ -9,14 +9,29 @@ const getIssueInfo = async( issueNumber: number ):Promise<Issue> => {
     return data;
 }
 
+const getIssueComments = async( issueNumber: number ):Promise<Issue[]> => {
+    await sleep(2);
+    const { data } = await githubApi.get<Issue[]>(`/issues/${ issueNumber }/comments`);
+    return data;
+}
+
 export const useIssue = ( issueNumber: number ) => {
   
     const issueQuery = useQuery(
-        ['issue', issueNumber ],
+        [ 'issue', issueNumber ],
         () => getIssueInfo( issueNumber ),
+    );
+    
+    const commentsQuery = useQuery(
+        [ 'issue', issueNumber, 'comments' ],
+        () => getIssueComments( issueQuery.data!.number ),
+        {
+            enabled: issueQuery.data !== undefined,
+        }
     );
   
     return{
         issueQuery,
+        commentsQuery,
     }
 }
