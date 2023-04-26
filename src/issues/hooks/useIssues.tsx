712834d5a16974e12f5903796c30c40e01366624
Hugo <hugo@boahstudio.com>
2023-04-26 08:01:00 -0300

    Pagination

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { githubApi } from "../../api/githubApi";
 import { Issue, State } from "../interfaces";
@@ -6,10 +7,11 @@ import { sleep } from "../../helpers/sleep";
 interface Props {
     state?: State;
     labels: string[];
+    page?: number;
 }
 
 
-const getIssues = async(  labels: string[] = [], state?: State ):Promise<Issue[]> => {
+const getIssues = async( { labels, state, page = 1 }: Props ):Promise<Issue[]> => {
     
     await sleep(2);
 
@@ -22,7 +24,7 @@ const getIssues = async(  labels: string[] = [], state?: State ):Promise<Issue[]
         params.append('labels', labelString );
     }
 
-    params.append('page', '1');
+    params.append('page', page?.toString());
     params.append('per_page', '5');
     
     const { data } = await githubApi.get<Issue[]>('/issues', { params });
@@ -31,14 +33,38 @@ const getIssues = async(  labels: string[] = [], state?: State ):Promise<Issue[]
 }
 
 export const useIssues = ({ state, labels }: Props) => {
+
+    const [page, setPage] = useState(1);
+    
+    useEffect(() => {
+        setPage(1);
+    }, [state, labels])
+    
   
     const issuesQuery = useQuery(
-        ['issues', { state, labels }],
-        () => getIssues( labels, state ),
+        ['issues', { state, labels, page }],
+        () => getIssues( {labels, state, page} ),
     );
+
+    const nextPage = () => {
+        if ( issuesQuery.data?.length === 0 ) return;
+        setPage ( page + 1 );
+    }
+
+    const prevPage = () => {
+        if ( page > 1) setPage ( page - 1 );
+    }
   
   
     return {
+        // Properties
         issuesQuery,
+
+        // Getter
+        page: issuesQuery.isFetching ? 'Loading...' : page,
+
+        // Methods
+        nextPage,
+        prevPage,
     }
 }
